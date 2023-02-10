58552bd9837bf33ca021c422d5c867a671fc7801
sinam7 <muwon777@naver.com>
2023-02-11 01:04:31 +0900

    git init

---
+++ src/test/java/com/sinam7/bulletinboard/BulletinboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinam7.bulletinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
